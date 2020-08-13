9642c2636b00cae190401187d9ca073feef74074
Eraville63661 <45951775+Eraville63661@users.noreply.github.com>
2020-08-13 17:20:15 +0200

    Thunkable + Lien
    Ajout de dossier/fichier dans thunkable, et redirection des liens

---
+++ Capsules/Thunkable/lien_1/Page_tuto.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  <title> Thunkable | Pause Cocoon </title>
+  <link rel="stylesheet" type="text/css" href="../../../Ressources_css/cubetto_prehistoire.css">
+  <link rel="stylesheet" type="text/css" href="../../../Ressources_css/css.css">
+  <link rel="stylesheet" type="text/css" href="../../../Ressources_css/menunavigation.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"/> </script>
+  <?php include'../../../menunavigationPersoScratch_Junior.php';?>
+
+  <div id="pageaImprimer">
+    <center>
+
+      <div id="contenu_droite">
+        <h2> Atelier Thunkable </h2>
+        <div class="trait"> </div>
+
+        <div class="container">
+
+          <p>XXXX</p><br>
+          <p>XXXX</p>
+
+          <div class="itemVideo"> <!-- Video 1-->
+            <iframe src="https://www.youtube.com/embed/OJAmgDQjS4Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>Créer un jeu</p>
+          </div>
+          
+          <div class="itemVideo"> <!-- Video 2-->
+            <iframe src="https://www.youtube.com/embed/hUR2LNhaK7g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>xxx</p>
+          </div>
+
+          <div class="itemVideo"> <!-- Video 3-->
+            <iframe src="https://www.youtube.com/embed/pKHm4SDXsAs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>xxx</p>
+          </div>
+
+          <div class="itemVideo"> <!-- Video 4-->
+            <iframe src="https://www.youtube.com/embed/hUR2LNhaK7g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>xxx</p>
+          </div>
+
+          <div class="itemVideo"> <!-- Video 5-->
+            <iframe src="https://www.youtube.com/embed/hUR2LNhaK7g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>xxx</p>
+          </div>
+
+          <div class="itemVideo"> <!-- Video 6-->
+            <iframe src="https://www.youtube.com/embed/hUR2LNhaK7g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>xxx</p>
+          </div>
+
+        <div class="choix">
+
+          <div class="lecon_prece">
+            <a href="Atelier_Thunkable.php">  <b> < </b> Leçon précédente </a>
+          </div>
+
+        </div>
+
+      </div>
+
+    </center>
+  </div>
+
+  <div id="imprimerPage">
+    <a href="#" onclick="imprimer('pageaImprimer');"><i class="fas fa-print"></i> </a>
+  </div>
+  <div class="haut">
+    <i class="fas fa-arrow-up"></i>
+  </div>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
+</html>
