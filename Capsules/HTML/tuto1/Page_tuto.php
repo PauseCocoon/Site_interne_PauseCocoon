3dbb0d6c3d6eb5b960ad55c5d592f3f406961380
Noichiboy <32422215+Noichiboy@users.noreply.github.com>
2020-08-18 14:42:41 +0200

    Création de l'onglet HTML et ses ateliers

---
+++ Capsules/HTML/tuto1/Page_tuto.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  <title> Programmer avec HTML 1 </title>
+  <link rel="stylesheet" type="text/css" href="../../../Ressources_css/cubetto_prehistoire.css">
+  <link rel="stylesheet" type="text/css" href="../../Ressources_css/css.css">
+  <link rel="stylesheet" type="text/css" href="../../../Ressources_css/menunavigation.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"/> </script>
+  <?php include'../../../menunavigationPersoScratch_Junior.php';?>
+
+  <div id="pageaImprimer">
+    <center>
+
+      <div id="contenu_droite">
+        <h2> Programmer avec HTML 1</h2>
+        <div class="trait"> </div>
+
+        <div class="container">
+
+          <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p><br>
+          <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+
+         <div class="row mt-4">
+            <div class="col-4 mt-4">
+              <iframe width="350px" height="350px" src="https://www.youtube.com/embed/lAOp-4uXqrY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <p>Video 1</p>
+            </div>
+            <div class="col-4 mt-4">
+              <iframe width="350px" height="350px" src="https://www.youtube.com/embed/pdtMUgnmRa4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <p>Video 2</p>
+            </div>
+            <div class="col-4 mt-4">
+              <iframe width="350px" height="350px" src="https://www.youtube.com/embed/D8PhglSB6hI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <p>Video 3</p>
+            </div>
+            <div class="col-4 mt-4">
+              <iframe width="350px" height="350px" src="https://www.youtube.com/embed/lAOp-4uXqrY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <p>Video 4</p>
+            </div>
+            <div class="col-4 mt-4">
+              <iframe width="350px" height="350px" src="https://www.youtube.com/embed/pdtMUgnmRa4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <p>Video 5</p>
+            </div>
+            <div class="col-4 mt-4">
+              <iframe width="350px" height="350px" src="https://www.youtube.com/embed/D8PhglSB6hI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <p>Video 6</p>
+            </div>
+          </div>
+
+
+        </div>
+
+
+
+        <div class="choix">
+
+          <div class="lecon_prece">
+            <a href="ProgrammerAvecHTML.php">  <b> < </b> Leçon précédente </a>
+          </div>
+
+        </div>
+
+      </div>
+
+    </center>
+  </div>
+
+  <div id="imprimerPage">
+    <a href="#" onclick="imprimer('pageaImprimer');"><i class="fas fa-print"></i> </a>
+  </div>
+  <div class="haut">
+    <i class="fas fa-arrow-up"></i>
+  </div>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
+</html>
